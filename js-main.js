32d8e2a82d848428250e2f16dc1c07b3b33ba494
Russell McCurdy <Russell.Alan.McCurdy@gmail.com>
2020-04-13 18:40:22 -0500

    first API functioning correctly

--- js-main.js
+++ js-main.js
@@ -3,8 +3,7 @@
 const baseURL = 'https://api.petfinder.com/v2/animals?organization=';
 const petFinderKey = 'hyDKKrJw4KAbju7PD8zWfVSWrwPpl0arsHp9w1KbGFtB8Rqw8l';
 const petFinderSecret = 'oMxNdOPgKqXJWXVVbKf2Vjfy0QwQoKSbFzFNWf7t';
-
-
+const wikiURL = ' https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch='
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -20,15 +19,34 @@ function displayResults(responseJson) {
       `<li><h2>${responseJson.animals[i].name}</h2>
       <h3>${responseJson.animals[i].breeds.primary}</h3>
       <div clas="dogPic">
-      <img src="${responseJson.animals[i].photos.map(x => x.medium)}" alt="A picure of a dog with ${responseJson.animals[i].breeds.primary} as it's primary breed">
+      <img src="${responseJson.animals[i].photos.map(x => x.medium)[0] ? responseJson.animals[i].photos.map(x => x.medium)[0] : getDefaultImage()}" alt="A picure of a dog with ${responseJson.animals[i].breeds.primary} as it's primary breed">
       </div>
-      <p>${responseJson.animals[i].description}</p>
-      </li>`
+      <p>${responseJson.animals[i].description ? responseJson.animals[i].description : getDefaultDescription()} </p>
+      </li>
+           <div>
+        <input type="submit" value="more Info" id="breedinfo${[i]}">
+            </div>
+    </li>`
     )};
   $('#results').removeClass('hidden');
 }
 
-function getPets(zip, type, breed, limit=20) {
+function getDefaultImage() {
+  return 'dog-1532627_640.png';
+}
+
+function getDefaultDescription() {
+  return 'No description. Please click the link below to get more info.'
+}
+
+/*function watchResults() {
+    $('#results-list').submit(event => {
+        let breed = $()
+        getWikiPets(breed);
+}*/
+
+
+function getPets(zip, type, breed, limit=50) {
 
     let url = 'https://api.petfinder.com/v2/animals?location=' + zip +'&' + 'limit=' + limit;
     url += (type != '' ?  '&type=' + type : '');
@@ -57,7 +75,6 @@ fetch('https://api.petfinder.com/v2/oauth2/token', {
 			'Content-Type': 'application/x-www-form-urlencoded'
 		}
     });
-
 }).then(function (resp) {
 
 	// Return the API response as JSON
@@ -72,12 +89,26 @@ fetch('https://api.petfinder.com/v2/oauth2/token', {
 }).catch(function (err) {
 
 	// Log any errors
-	console.log('something went wrong', err);
+    console.log('something went wrong', err);
+fetch ('')
 })}
 
+function getWikiPets(breed) {
+    let searchURL= wikiURL + breed;
+    fetch (searchURL
+    ).then(function (resp) {
+        return resp.json();
+    }).then(function (data) {
+        console.log(data)
+        displayWiki(data);
+    }).catch(function (err) {
+        console.log('something went wrong', err)
+    }) 
+}
 
+function displayWiki(breed) {
 
-
+}
 function watchForm() {
   $('#js-form').submit(event => {
     event.preventDefault();
